2e05d82e0bdccda23764fac761d2ae7781c25794
Madis Nõmme <me@mad.is>
2022-08-10 13:57:42 +0200

    Add zkSync greeter example

---
+++ greeter-example/hardhat.config.ts
@@ -0,0 +1,29 @@
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+
+module.exports = {
+  zksolc: {
+    version: "1.1.0",
+    compilerSource: "docker",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+      experimental: {
+        dockerImage: "matterlabs/zksolc",
+      },
+    },
+  },
+  zkSyncDeploy: {
+    zkSyncNetwork:   "https://zksync2-testnet.zksync.dev",
+    ethNetwork: "https://eth-goerli.alchemyapi.io/v2/" // "https://goerli.infura.io/v3/ceea6e2f327843a1a82a2b565ae2d0ef" // "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+  },
+  networks: {
+    hardhat: {
+      zksync: true,
+    },
+  },
+  solidity: {
+    version: "0.8.10",
+  },
+};
